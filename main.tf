95ad5204aa1266fe3d1e7c53e6b3a12158c7e6c0
Mattias Nixell <mattias.nixell@gmail.com>
2023-05-22 14:26:51 +0300

    Add S3 ownership controls for new AWS defaults (#18)

--- main.tf
+++ main.tf
@@ -19,7 +19,18 @@ resource "aws_s3_bucket_versioning" "state" {
   }
 }
 
+resource "aws_s3_bucket_ownership_controls" "state" {
+  bucket = aws_s3_bucket.state.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_acl" "state" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.state,
+  ]
+  
   bucket = aws_s3_bucket.state.id
   acl    = "private"
 }
