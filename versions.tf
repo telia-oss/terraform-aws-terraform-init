0e4c3c30493bd711dd3c91d199dddb30487e0faf
Jitendra Yadav <jitendra.yadav@teliacompany.com>
2023-08-29 10:59:32 +0200

    update to tf1.5

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.14"
+  required_version = ">= 1.5"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.75, < 5.0"
+      version = ">= 5.0"
     }
   }
 }
